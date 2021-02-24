36b64aa5c5f7772b6d816daef1adc7b5935f302d
Dimitri Paillard <dimitri.p53@live.fr>
2021-02-24 14:38:52 +0100

    Add files via upload

---
+++ precache-manifest.fd2ad5505429b82fc63bca3303972e16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfceed74b4117ea5002b7e160f253ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b41a499ac6fd7ec32e",
+    "url": "/static/css/main.2de5543c.chunk.css"
+  },
+  {
+    "revision": "a044e25f2959aecfa969",
+    "url": "/static/js/2.c13792ea.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c13792ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b41a499ac6fd7ec32e",
+    "url": "/static/js/main.b8ae82df.chunk.js"
+  },
+  {
+    "revision": "dcc534e5adf1fce6f480",
+    "url": "/static/js/runtime-main.5f677427.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
